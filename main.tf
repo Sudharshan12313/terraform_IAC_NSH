877e1683a879cc163f9cbb24a0cd72ac58ba7042
Venkat1406-sudo <venkatgajulapalli7@gmail.com>
2025-01-15 13:58:32 +0530

    Main.tf file added

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "test" {
+  bucket = "test_bucket_nsh"
+  acl    = "private"
+}
+
+resource "aws_instance" "test_instance" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "TO-BE-DELETED"
+  }
+}
